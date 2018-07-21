593d6c86e85c7a9f475ad1779518a89b13ce88fe
liumapp <liumapp.com@gmail.com>
2018-07-21 15:09:50 +0800

    add hello stream main class

---
+++ stream-service/hello-stream/src/main/java/com/liumapp/demo/cloud/stream/service/hello/HelloStreamMain.java
@@ -0,0 +1,20 @@
+package com.liumapp.demo.cloud.stream.service.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liumapp
+ * @file HelloStreamMain.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/21/18
+ */
+@SpringBootApplication
+public class HelloStreamMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloStreamMain.class, args);
+    }
+
+}
