0db36521ec4ca28966459a6d1b7316ba7f236591
liumapp <liumapp.com@gmail.com>
2018-07-28 22:27:20 +0800

    rm package info

---
+++ stream-service/first-group/src/main/java/com/liumapp/demo/cloud/stream/service/group/first/FirstGroupMain.java
@@ -0,0 +1,20 @@
+package com.liumapp.demo.cloud.stream.service.group.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liumapp
+ * @file FirstGroupMain.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/7/28
+ */
+@SpringBootApplication
+public class FirstGroupMain {
+
+    public static void main (String[] args) {
+        SpringApplication.run(FirstGroupMain.class, args);
+    }
+
+}
--- stream-service/first-group/src/main/java/com/liumapp/demo/cloud/stream/service/group/first/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.liumapp.demo.cloud.stream.service.group.first;
