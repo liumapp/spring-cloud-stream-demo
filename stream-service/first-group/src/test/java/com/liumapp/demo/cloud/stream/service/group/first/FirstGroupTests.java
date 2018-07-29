32f5f4e47b78c48fdfb8100799395e2aa5b7a26b
liumapp <liumapp.com@gmail.com>
2018-07-30 07:02:09 +0800

    add config resources

---
+++ stream-service/first-group/src/test/java/com/liumapp/demo/cloud/stream/service/group/first/FirstGroupTests.java
@@ -0,0 +1,25 @@
+package com.liumapp.demo.cloud.stream.service.group.first;
+
+import com.liumapp.demo.cloud.stream.service.group.first.sender.SinkSender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Import({FirstGroupMain.class})
+@WebAppConfiguration
+public class FirstGroupTests {
+
+    @Autowired
+    private SinkSender sinkSender;
+
+    @Test
+    public void contextLoads () {
+        sinkSender.output().send(MessageBuilder.withPayload("From SinkSender").build());
+    }
+
+}
