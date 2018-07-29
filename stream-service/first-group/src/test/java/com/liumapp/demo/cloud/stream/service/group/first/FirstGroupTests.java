75d3ed5297181466c29f68e2c964973324ddc150
liumapp <liumapp.com@gmail.com>
2018-07-30 07:06:55 +0800

    test success

--- stream-service/first-group/src/test/java/com/liumapp/demo/cloud/stream/service/group/first/FirstGroupTests.java
+++ stream-service/first-group/src/test/java/com/liumapp/demo/cloud/stream/service/group/first/FirstGroupTests.java
@@ -4,13 +4,13 @@ import com.liumapp.demo.cloud.stream.service.group.first.sender.SinkSender;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@Import({FirstGroupMain.class})
+@SpringBootTest(classes = {FirstGroupMain.class})
 @WebAppConfiguration
 public class FirstGroupTests {
 
