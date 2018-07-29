195770f437b96d75a60a8f41ed6c58570db283c8
liumapp <liumapp.com@gmail.com>
2018-07-30 07:11:32 +0800

    rm unnecessary files

--- stream-service/first-group/src/test/java/com/liumapp/demo/cloud/stream/service/group/first/FirstGroupTests.java
+++ stream-service/first-group/src/test/java/com/liumapp/demo/cloud/stream/service/group/first/FirstGroupTests.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -17,9 +18,17 @@ public class FirstGroupTests {
     @Autowired
     private SinkSender sinkSender;
 
+    @Autowired
+    private MessageChannel input;
+
+    @Test
+    public void sendText () {
+        sinkSender.output().send(MessageBuilder.withPayload("From SinkSender").build());
+    }
+
     @Test
     public void contextLoads () {
-        sinkSender.output().send(MessageBuilder.withPayload("From SinkSender").build());
+
     }
 
 }
