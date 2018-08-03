27ae98836d7959b15b563fdc42b36108a2eeb09c
liumapp <liumapp.com@gmail.com>
2018-08-03 09:18:50 +0800

    add sink receiver in second group

---
+++ stream-service/second-group/src/main/java/com/liumapp/demo/cloud/stream/service/group/second/receiver/SinkReceiver.java
@@ -0,0 +1,27 @@
+package com.liumapp.demo.cloud.stream.service.group.second.receiver;
+
+import com.liumapp.demo.cloud.stream.service.group.second.sender.SinkSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+/**
+ * @author liumapp
+ * @file SinkReceiver.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/8/3
+ */
+@EnableBinding(value = {Sink.class, SinkSender.class})
+public class SinkReceiver {
+
+    private static Logger logger = LoggerFactory.getLogger(SinkReceiver.class);
+
+    @StreamListener(Sink.INPUT)
+    public void receive (Object payload) {
+        logger.info("Received: " + payload);
+    }
+
+}
